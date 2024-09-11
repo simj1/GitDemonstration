945fe53dd5b0c1cdfb9a91437273fbcd786ed75a
simj1 <44676171+simj1@users.noreply.github.com>
2024-09-11 08:05:29 +0000

    fixed plot issue

--- analysis_functions/plot_data.py
+++ analysis_functions/plot_data.py
@@ -1,15 +1,15 @@
 from matplotlib import pyplot as plt
 
-def plot_data(df, col_x, col_y):
+def plot_data(df, col_x, col_y, filename="default.png"):
 
     df_plot = df[[col_x, col_y]].groupby([col_x]).agg(["sum", "count"]).reset_index()
     print(df_plot.columns)
     
     fig, ax = plt.subplots(figsize=(10, 10))
 
-    ax.bar(df_plot[col_x], df_plot[("Survived", "sum")])
-    ax.bar(df_plot[col_x], df_plot[("Survived", "count")])
+    ax.bar(df_plot[col_x] - 0.3, df_plot[("Survived", "sum")], width=0.25)
+    ax.bar(df_plot[col_x]+0.05, df_plot[("Survived", "count")], width=0.25)
     ax.set_xlabel(col_x)
     ax.set_ylabel(col_y)
 
-    plt.show()
+    plt.savefig(filename)
